b0695a77680c775ee9a50730db3879adeb844fb4
Luke <luke.goddard44@gmail.com>
2022-09-05 18:31:44 +0100

    Add test suite for board placement

--- lib/peices/peice.rb
+++ lib/peices/peice.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Peice
+  attr_reader :color
   def initialize(color)
     @color = color
   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,10 +13,26 @@ describe Board do
 
       it 'has two pawns in each column' do
         result = test_board.board.all? do |column|
-          2 == column.reduce { |sum, square| sum += 1 if square.is_a?(Pawn) }
+          pawn_count = column.reduce(0) do |sum, position|
+            sum += 1 if position.is_a?(Pawn)
+            sum
+          end
+          pawn_count == 2
         end
 
         expect(result).to eq(true)
+      end
+
+      it 'creates a black rook in the top left position' do
+        rook = test_board.board[0][7]
+        result = rook.color == :black && rook.is_a?(Rook)
+        expect(result).to be(true)
+      end
+
+      it 'creates a white bishop in correct bottom left positon' do
+        bishop = test_board.board[2][0]
+        result = bishop.color == :white && bishop.is_a?(Bishop)
+        expect(result).to be(true)
       end
     end
   end
