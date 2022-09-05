d9c3d39a0d635eb1ff4879a26db36f67237adb4f
Luke <luke.goddard44@gmail.com>
2022-09-05 16:36:10 +0100

    Add board creating method

--- lib/board.rb
+++ lib/board.rb
@@ -3,4 +3,31 @@
 require_relative 'peice'
 
 class Board
+  attr_accessor :board
+  
+  def initialize
+    @board = create_board 
+  end
+
+  private
+
+  def create_board
+    board = []
+    ticker = true
+
+    8.times do
+      column = []
+      8.times do
+        column.push({color: ticker ? 'white' : 'black', peice: nil})
+        ticker = !ticker
+      end
+
+      board.push(column)
+      ticker = !ticker
+    end
+  end
+
+  def fill_board
+    @board.map!
+  end
 end
