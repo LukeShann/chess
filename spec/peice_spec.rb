789f960ce48212e74e5edefa46621ff83fc670e1
Luke <luke.goddard44@gmail.com>
2022-09-06 20:41:17 +0100

    Add test suite for .can_move?

---
+++ spec/peice_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../lib/peices/peice'
+require_relative '../lib/peices/knight'
+require_relative '../lib/board'
+
+describe Peice do
+  subject(:test_knight) { Knight.new(:black, [0, 0]) }
+  let(:test_board) { Board.new.board }
+  before { test_board[0][0] = test_knight }
+  
+  describe '.can_move?' do
+    context 'when open' do
+      it 'returns true' do
+        expect(test_knight.can_move?(test_board)).to be(true)
+      end
+    end
+
+    context 'when there are no possible moves' do
+      before do
+         test_board[1][2] = Knight.new(:black, [1, 2])
+         test_board[2][1] = Knight.new(:black, [2, 1])
+      end
+
+      it 'returns false' do
+        expect(test_knight.can_move?(test_board)).to be(false)
+      end
+    end
+  end
+end
