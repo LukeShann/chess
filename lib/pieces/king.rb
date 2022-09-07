6bcc7ecc1f2413a1f86078db501a076e0681d41a
Luke <luke.goddard44@gmail.com>
2022-09-07 15:51:11 +0100

    Add king movement

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -4,8 +4,18 @@
 require_relative 'piece'
 
 class King < Piece
+  @@move_directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
   def initialize(color, coords)
     super(color, coords)
     @ascii = ['♔', '♚']
+  end
+
+  def possible_moves(board)
+    new_positions = @@move_directions
+    .map { |move| next_coords(@coords, move)}
+    .reject(&:nil?)
+    
+  #TODO: filter out moves that would put king in check
+  filter_moves(board, new_positions)
   end
 end
