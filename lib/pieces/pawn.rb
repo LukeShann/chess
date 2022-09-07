d330e7917109b7d14683d881af2b38a1e6a4b886
Luke <luke.goddard44@gmail.com>
2022-09-07 13:37:18 +0100

    Add pawn movement

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -7,17 +7,29 @@ class Pawn < Piece
   def initialize(color, coords)
     super(color, coords)
     @ascii = ['♙', '♟']
-    @is_first_move = true
+    if color == :black
+      @move_directions = [[0, -1], [0, -2]]
+      @take_directions = [[-1, -1], [1, -1]]
+    else
+      @move_directions = [[0, 1], [0, 2]]
+      @take_directions = [[-1, 1], [1, 1]]
+    end
   end
 
   def possible_moves(board)
+    move_positions = @move_directions \
+      .map { |move| next_coords(@coords, move)} \
+      .select { |move| board[move.first][move.last].nil? }
 
-    # from Knight:
-    # new_positions = @move_directions.map { |move| next_coords(@coords, move)}
-    # filter_moves(board, new_positions
+    take_positions = @take_directions \
+      .map { |move| next_coords(@coords, move)} \
+      .reject { |move| board[move.first][move.last].nil? }
+
+    move_positions + take_positions
   end
 
-  def move()
-    # is_first_move to false
+  def move_coords(coords)
+    super(coords)
+    @move_directions.pop if @move_directions.size > 1
   end
 end
